1b61e6f9d1dacd17b189555cc2673fc67dd4bfa6
Bren <bpgit503@gmail.com>
2025-04-12 13:43:12 +0200

    first commit

---
+++ src/test/java/com/devbp/ecommerce_api/EcommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbp.ecommerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
